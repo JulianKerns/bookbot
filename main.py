9ff61801b0be07cd83d57c354d8f07d4593bb5cb
JulianKerns <j.kernstock@gmx.at>
2024-01-15 13:57:50 +0100

    with the help of the course available on boot.dev I my first small project. It counts the words in a given String and also gives out the usage count of each character. The programm gives out a small book report.

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ def main():
     book_path = "books/frankenstein.txt.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    count = get_count_characters(text)
+    report = get_book_report(count)
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
+
+    print()
+    for item in report:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item["char"]}' character has been found {item["num"]} times")
+    print("--- End Report ---")
+    
+    
 
 def get_num_words(text):
     words = text.split()
@@ -12,7 +24,30 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-    
+
+def get_count_characters(text):
+    alphabeth =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    count_dict = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in count_dict:
+            count_dict[lowered] += 1
+        else:
+            count_dict[lowered] = 1
+                
+    return count_dict
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def get_book_report(count):
+    sorted_list= []
+    for ch in count:
+        sorted_list.append({"char":ch,"num":count[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 
 main()
